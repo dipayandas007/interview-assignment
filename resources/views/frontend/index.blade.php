bc39b05fba83d6cd969f8221ecf65ee9b8cfcd86
Dipayan Das <dipayandas007@gmail.com>
2022-02-17 20:19:29 +0530

    uplodaing assignment code

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -21,20 +21,4 @@
             <example-component></example-component>
         </div><!--col-->
     </div><!--row-->
-
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fab fa-font-awesome-flag"></i> Font Awesome @lang('strings.frontend.test')
-                </div>
-                <div class="card-body">
-                    <i class="fas fa-home"></i>
-                    <i class="fab fa-facebook"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-pinterest"></i>
-                </div><!--card-body-->
-            </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
 @endsection
